f3416235a9efd4bf61e709bf3ad27b8b035769dc
Ronald Tomaz <ronaldtmprofile@gmail.com>
2022-07-13 10:09:34 -0300

    added component Comment

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -1,3 +1,4 @@
+import { Comment } from '../Comment'
 import styles from './styles.module.css'
 
 export function Post() {
@@ -35,6 +36,11 @@ export function Post() {
                 <button type="submit">Publicar</button>
                 
             </form>    
+
+            
+            <div className={styles.commentList}>
+                <Comment />
+            </div>
         </article>
     )
 }
